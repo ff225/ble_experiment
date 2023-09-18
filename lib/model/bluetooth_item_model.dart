ba44af1bce66789a71489f64283690f4e3bc1bbd
Francesco Franco <frafranco16@gmail.com>
2023-09-18 17:55:57 +0200

    aggiunto campo data

--- lib/model/bluetooth_item_model.dart
+++ lib/model/bluetooth_item_model.dart
@@ -2,9 +2,10 @@ import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
 
 class BluetoothModel {
   BluetoothModel(this.id, this.name,
-      {this.isConnected = DeviceConnectionState.disconnected});
+      {this.isConnected = DeviceConnectionState.disconnected, this.data = 0});
 
   final String id;
   final String name;
   DeviceConnectionState isConnected;
+  dynamic data;
 }
